28820e34ddc825ca5652e83efe25b39d514f84ea
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-05-26 22:59:50 +0200

    updated hero CLASS

--- models/Hero.java
+++ models/Hero.java
@@ -1,4 +1,13 @@
 package com.company.main.models;
 
 public class Hero {
+    public int Attack;
+    public int Defense;
+    public int Hp; //HealthPack
+
+    public Hero(int a, int d, int hp){
+        this.Attack = a;
+        this.Defense = d;
+        this.Hp = hp;
+    }
 }
