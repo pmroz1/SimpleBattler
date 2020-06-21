6453c9d053b9ddfe4ea84c912cc7aa52b291a40b
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-06-21 15:28:13 +0200

    alpha v1.03

---
+++ DOKUMENTACJA/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.company.main"},{"l":"com.company.main.models"},{"l":"com.company.main.services"},{"l":"com.company.main.shared"},{"l":"com.company.main.ui"}]
